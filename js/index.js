585bff4fe3a5908b8cb5e39f7ef413bd0b714e8f
leonardoavila <leonardofabricioavila@gmail.com>
2025-09-29 14:02:48 -0400

    pequenos ajustes

--- js/index.js
+++ js/index.js
@@ -1,10 +1,11 @@
 
 import initWebSpeech from "./webSpeech.js";
 import initStatusAnim from "./recStatusAnim.js";
-
+import initFloatScreen from "./floatScreen.js";
 import initBodyAnimation from "./bodyAnimations.js";
 
 // initWebSpeech();
 
 initBodyAnimation();
 initStatusAnim();
+initFloatScreen();
